1910c3be3bb44161b646a588d9393b6a76862dac
Amin <amindalvand97@gmail.com>
2024-12-22 13:35:24 +0330

    Content Management Dash - https://trello.com/c/aRhG9C8q FULL@LAB

--- __init__.py
+++ __init__.py
@@ -6,5 +6,9 @@ from .elements.form import Form
 from .elements.form_element_upload_image import FormElementUploadImage
 from .elements.page import Page
 from .elements.text_field import TextField
+from .elements.wave_chart import WaveChart
+from .elements.circular_distribution import CircularDistribution
+from .elements.chart_comparison import ChartComparison
+from .elements.switch import Switch
 
 
