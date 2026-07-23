3ef987612b419b96a6f8249ccad8a45880fb9278
Hesam Jaferi <hesam.jaferi@gmail.com>
2026-07-23 07:04:26 +0330

    fix: donut chart element

--- src/dashpaan/elements/donut_chart.py
+++ src/dashpaan/elements/donut_chart.py
@@ -1,22 +1,22 @@
-from src.dashpaan.elements.base import Element
+from dashpaan.elements.base import Element
 
 
 class DonutChart(Element):
     kind = "donut-chart"
 
     title = ""
+    size = "1*1"
     series = {}
     labels = []
-    options = {}
 
     def json(self):
         return {
             **super(DonutChart, self).json(),
             "version": self.version,
             "title": self.title,
+            "size": self.size,
             "series": self.series,
-            "labels": self.labels,
-            "options": self.options
+            "labels": self.labels
         }
 
     @classmethod
