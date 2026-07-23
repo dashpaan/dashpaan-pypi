b78999e98f0aa5a268e6b70ecd34f7ab48368e04
Hesam Jaferi <hesam.jaferi@gmail.com>
2026-07-23 07:05:23 +0330

    feat: navigation class

--- src/dashpaan/elements/navigation.py
+++ src/dashpaan/elements/navigation.py
@@ -1,17 +1,64 @@
-from src.dashpaan.elements.base import Element
+import json
+import uuid
+
+from dashpaan.elements.base import Element
 
 
 class Navigation(Element):
     kind = "navigation"
 
-    items = []
+    elements = []
 
     def json(self):
         return {
             **super(Navigation, self).json(),
-            "items": self.items
+            "items": self.elements
         }
 
     @classmethod
     def from_json(cls, obj):
         return Navigation(**obj)
+
+    class Item(Element):
+        kind = "nav-item"
+
+        id = uuid.uuid4().hex
+        title = ""
+        icon = None
+        action = None
+
+        def json(self):
+            return {
+                **super(Navigation.Item, self).json(),
+                "id": self.id,
+                "title": self.title,
+                "type": "item",
+                "icon": self.icon,
+                "action": self.action.json() if self.action else None
+            }
+
+        @classmethod
+        def from_json(cls, obj):
+            return Navigation.Item(**obj)
+
+    class Group(Element):
+        kind = "nav-group"
+
+        id = uuid.uuid4().hex
+        title = ""
+        icon = None
+        elements = []
+
+        def json(self):
+            return {
+                **super(Navigation.Group, self).json(),
+                "id": self.id,
+                "title": self.title,
+                "type": "group",
+                "icon": self.icon,
+                "items": self.elements
+            }
+
+        @classmethod
+        def from_json(cls, obj):
+            return Navigation.Group(**obj)
