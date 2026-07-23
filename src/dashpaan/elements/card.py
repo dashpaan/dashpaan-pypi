78a14c4c43521f7fa97ebcfff50411cf83f1fd7f
Hesam Jaferi <hesam.jaferi@gmail.com>
2026-07-23 07:02:30 +0330

    feat: card element

--- src/dashpaan/elements/card.py
+++ src/dashpaan/elements/card.py
@@ -1,16 +1,20 @@
-from src.dashpaan.elements.base import Element
+from dashpaan.elements.base import Element
 
 
 class Card(Element):
     kind = "card"
 
     icons = None
+    badges = None
     cover = None
     size = None
     title = None
     boxes = None
     menu = None
+    people = None
+    profile = None
     description = None
+    action = None
 
     def json(self):
         return {
@@ -18,10 +22,14 @@ class Card(Element):
             "title": self.title,
             "size": self.size,
             "icons": self.icons,
+            "badges": self.badges,
             "boxes": self.boxes,
             "menu": self.menu,
+            "people": self.people,
+            "profile": self.profile,
             "description": self.description,
-            "cover": self.cover
+            "cover": self.cover,
+            "action": self.action.json() if self.action else None,
         }
 
     @classmethod
