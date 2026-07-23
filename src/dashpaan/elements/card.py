0cf0da015241dfa5e5283c89944b926dc708829d
Hesam Jaferi <hesam.jaferi@gmail.com>
2026-07-23 11:27:26 +0330

    fix: card element

--- src/dashpaan/elements/card.py
+++ src/dashpaan/elements/card.py
@@ -21,12 +21,13 @@ class Card(Element):
             **super(Card, self).json(),
             "title": self.title,
             "size": self.size,
+            "cover": self.cover,
             "icons": self.icons,
-            "badges": self.badges,
+            "badges": [x.json() for x in self.badges] if self.badges else None,
             "boxes": self.boxes,
-            "menu": self.menu,
-            "people": self.people,
-            "profile": self.profile,
+            "menu": [x.json() for x in self.menu] if self.menu else None,
+            "people": [x.json() for x in self.people] if self.people else None,
+            "profile": self.profile.json() if self.profile else None,
             "description": self.description,
             "cover": self.cover,
             "action": self.action.json() if self.action else None,
@@ -35,3 +36,62 @@ class Card(Element):
     @classmethod
     def from_json(cls, obj):
         return Card(**obj)
+
+    class Profile(Element):
+        kind = "card-profile"
+
+        name = None
+        description = None
+        image = None
+        action = None
+
+        def json(self):
+            return {
+                **super(Card.Profile, self).json(),
+                "name": self.name,
+                "description": self.description,
+                "image": self.image,
+                "action": self.action.json() if self.action else None,
+            }
+
+        @classmethod
+        def from_json(cls, obj):
+            return Card.Profile(**obj)
+
+    class Badge(Element):
+        kind = "card-badge"
+
+        color = None
+        icon = None
+        title = None
+        action = None
+
+        def json(self):
+            return {
+                **super(Card.Badge, self).json(),
+                "color": self.color,
+                "icon": self.icon,
+                "title": self.title,
+                "action": self.action.json() if self.action else None,
+            }
+
+        @classmethod
+        def from_json(cls, obj):
+            return Card.Badge(**obj)
+
+    class MenuItem(Element):
+        kind = "card-menu-item"
+
+        name = None
+        action = None
+
+        def json(self):
+            return {
+                **super(Card.MenuItem, self).json(),
+                "name": self.name,
+                "action": self.action.json() if self.action else None,
+            }
+
+        @classmethod
+        def from_json(cls, obj):
+            return Record.MenuItem(**obj)
