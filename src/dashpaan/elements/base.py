6231ebe2d96c550968a98d0c36fe89f9227abbc1
Hesam Jaferi <hesam.jaferi@gmail.com>
2026-07-23 07:06:47 +0330

    feat: use args as inline elements

--- src/dashpaan/elements/base.py
+++ src/dashpaan/elements/base.py
@@ -2,10 +2,13 @@ class Element:
     kind = None
     version = "v1"
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             if hasattr(self, key):
                 setattr(self, key, value)
+
+        if args and hasattr(self, "elements"):
+            self.elements = [x for x in args if x]
 
     def json(self):
         return {
